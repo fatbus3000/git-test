6aca756502c41ad17a0dd5262717e956459d6369
fatbus3000 <fatbus3000-github@yahoo.com>
2017-03-31 23:14:28 +0200

    restructured main and added import

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+import sys
+import os
 
 def say_goodbye(name):
 	print("Goodbye!", name)
 
-if __name__ == "__main__":
+def main():
 	print("hello world")
 	say_goodbye('John')
+
+if __name__ == "__main__":
+	main()
